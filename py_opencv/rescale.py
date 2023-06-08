54fa595cf7b2177d4bc17aa279497bf43cdba61a
Angel Cheng <angelhyc@umich.edu>
2023-06-08 13:58:55 -0400

    python version changeres

--- py_opencv/rescale.py
+++ py_opencv/rescale.py
@@ -2,20 +2,28 @@ import cv2 as cv
 
 
 def rescaleFrame(frame, scale=0.75):
+    # works for Photos, Videos, Live Videos
     width = int(frame.shape[1]*scale)
     height = int(frame.shape[0]*scale)
     dimensions = (width, height)
 
     return cv.resize(frame, dimensions, interpolation=cv.INTER_AREA)
 
+def changeRes(width, height):
+    # only works for Live Videos
+    capture.set(3, width)
+    capture.set(4, height)
+
+
+
+
 """ Image Resize """
-img = cv.imread('photos/dog2.jpg')
-rescale_im = rescaleFrame(img, 0.2)
+# img = cv.imread('photos/dog2.jpg')
+# rescale_im = rescaleFrame(img, 0.2)
 
-cv.imshow('Resize Dog', rescale_im)
-cv.imshow('Dog', img)
-cv.waitKey(0)
-
+# cv.imshow('Resize Dog', rescale_im)
+# cv.imshow('Dog', img)
+# cv.waitKey(0)
 
 """ Video Resize """
 # capture = cv.VideoCapture('videos/vid1.mp4') # web cam referenced by int 0 
